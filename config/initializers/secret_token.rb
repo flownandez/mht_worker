6871a4662fd8fa741d1b19b1ebe3d5b7996a3f79
Chris Fernandez <chris2fernandez@gmail.com>
2014-02-13 15:47:15 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MhtWorker::Application.config.secret_key_base = 'a33c5e332e57715c3b56bf4ebb9792a910a8d1aa3fc3bef49832c8b5a768dcd0d344ccbf39c4401a16bba865bec3aea5e85ea0e5ebcf08b94e2d939ae4989a0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MhtWorker::Application.config.session_store :cookie_store, key: '_mht_worker_session'
